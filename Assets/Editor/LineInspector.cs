c7890504d7bb8b5c2977c074736e0130e128fb3c
Osinko <yoshinari_m@msn.com>
2014-09-25 23:12:45 +0900

    no message

---
+++ Assets/Editor/LineInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+		void OnSceneGUI ()
+		{
+				Line line = target as Line;
+		
+				Handles.color = Color.white;
+				Handles.DrawLine (line.p0, line.p1);
+		}
+}
---
+++ Assets/Line.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line : MonoBehaviour
+{
+		public Vector3 p0, p1;
+
+		void Start ()
+		{
+	
+		}
+	
+}
