0d224e3af8d53d68b17e1533d6898e7c87b328bd
Osinko <yoshinari_m@msn.com>
2014-09-26 01:50:45 +0900

    no message

--- Assets/Editor/LineInspector.cs
+++ Assets/Editor/LineInspector.cs
@@ -8,8 +8,19 @@ public class LineInspector : Editor
 		void OnSceneGUI ()
 		{
 				Line line = target as Line;
-		
+
+				Transform handleTransform = line.transform;
+
+				Quaternion handleRotaiton = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+				//unityツールのpivot回転設定がローカルならLineオブジェクトの回転にする。それ以外は初期値に
+
+				Vector3 p0 = handleTransform.TransformPoint (line.p0);	//ワールド座標にTransformのローカルを加算
+				Vector3 p1 = handleTransform.TransformPoint (line.p1);
+
 				Handles.color = Color.white;
-				Handles.DrawLine (line.p0, line.p1);
+				Handles.DrawLine (p0, p1);						//ラインを表示
+				Handles.DoPositionHandle (p0, handleRotaiton);	//位置ハンドルを表示
+				Handles.DoPositionHandle (p1, handleRotaiton);
 		}
+
 }
